7aab0deee62ffe422d5e6cd72b2b2fe2ba6bd9ce
Su-Cong <55074486+Su-Cong@users.noreply.github.com>
2020-08-09 11:29:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,31 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <complex>
 #include <chrono>
-
+#include <omp.h>
+#include<immintrin.h>
+#include<xmmintrin.h>
 using namespace std;
 
-typedef complex<double> Complex;
+#define DOUBLE_SIZE 8
+#define LINE_SIZE 20
 typedef chrono::high_resolution_clock Clock;
 
 const int m=1638400;	// DO NOT CHANGE!!
 const int K=100000;	// DO NOT CHANGE!!
 
-double logDataVSPrior(const Complex* dat, const Complex* pri, const double* ctf, const double* sigRcp, const int num, const double disturb0);
+double logDataVSPrior(const double* dat_real,const double* dat_imag, const double* pri_real,const double* pri_imag, const double* ctf, const double* sigRcp, const int start,const int chunkSize, const double disturb,const int chunkId);
 
 int main ( int argc, char *argv[] )
-{ 
-    Complex *dat = new Complex[m];
-    Complex *pri = new Complex[m];
-    double *ctf = new double[m];
-    double *sigRcp = new double[m];
-    double *disturb = new double[K];
+{   
+    double *dat_real = (double*)_mm_malloc(DOUBLE_SIZE*m,64),*dat_imag=(double*)_mm_malloc(DOUBLE_SIZE*m,64);
+    double *pri_real = (double*)_mm_malloc(DOUBLE_SIZE*m,64),*pri_imag=(double*)_mm_malloc(DOUBLE_SIZE*m,64);
+    double *ctf = (double*)_mm_malloc(DOUBLE_SIZE*m,64);
+    double *sigRcp = (double*)_mm_malloc(DOUBLE_SIZE*m,64);
+    double *disturb = (double*)_mm_malloc(DOUBLE_SIZE*K,64);
+    double *result=(double*)_mm_malloc(DOUBLE_SIZE*m,64);
     double dat0, dat1, pri0, pri1, ctf0, sigRcp0;
+
 
     /***************************
      * Read data from input.dat
@@ -43,8 +47,10 @@ int main ( int argc, char *argv[] )
     while( !fin.eof() ) 
     {
         fin >> dat0 >> dat1 >> pri0 >> pri1 >> ctf0 >> sigRcp0;
-        dat[i] = Complex (dat0, dat1);
-        pri[i] = Complex (pri0, pri1);
+        dat_real[i] =dat0;
+        dat_imag[i]=dat1;
+        pri_real[i] =pri0;
+        pri_imag[i]= pri1;
         ctf[i] = ctf0;
         sigRcp[i] = sigRcp0;
         i++;
@@ -71,7 +77,12 @@ int main ( int argc, char *argv[] )
      * main computation is here
      * ************************/
     auto startTime = Clock::now(); 
-
+//    FILE* fp=fopen("result.dat","w");
+//    if(fp==NULL){
+//        cout << "Error opening file for result" << endl;
+//        exit(1);
+//    }
+    
     ofstream fout;
     fout.open("result.dat");
     if(!fout.is_open())
@@ -79,38 +90,77 @@ int main ( int argc, char *argv[] )
          cout << "Error opening file for result" << endl;
          exit(1);
     }
+    
+    int thread_num=1,chunkSize=512,chunkNum=m/chunkSize,reduce_chunk=0;
+    double* resBuffer=nullptr; 
+        
+    #pragma omp parallel
+    { 
+        #pragma omp single
+        {
+            thread_num=omp_get_num_threads();
+            resBuffer=(double*)_mm_malloc(DOUBLE_SIZE*thread_num*K,64);
+        }
+        int tid=omp_get_thread_num();
+        #pragma omp for schedule(guided)
+        for(int chunkId=0;chunkId<chunkNum;chunkId++){
+            int start=chunkSize*chunkId;
+            if(chunkId==chunkNum-1)
+                chunkSize=m-start;
+            for (unsigned int t = 0; t < K; t++)
+            {
+                resBuffer[tid*K+t]+=logDataVSPrior(dat_real,dat_imag, pri_real,pri_imag, ctf, sigRcp,start,chunkSize, disturb[t],chunkId);
+            }
+        }
 
-#pragma omp parallel for schedule(static) num_threads(96)
+        #pragma omp for schedule(guided)
+        for(int t=0;t<K;t++){
+            #pragma ivdep
+            #pragma unroll(4)
+            for(int i=0;i<thread_num;i++){
+                result[t]+=resBuffer[i*K+t];
+            }
+        }
+    }
+
+//    for(unsigned int t=0;t<K;t++){
+//        fprintf(fp,"%d: %.6g\n",t+1,result[t]);
+//    }
+//    fclose(fp);
+
     for(unsigned int t = 0; t < K; t++)
     {
-        double result = logDataVSPrior(dat, pri, ctf, sigRcp, m, disturb[t]);
-        fout << t+1 << ": " << result << endl;
+        fout << t+1 << ": " << result[t] << endl;
     }
     fout.close();
-
-    auto endTime = Clock::now(); 
+	
+	auto endTime = Clock::now(); 
 
     auto compTime = chrono::duration_cast<chrono::microseconds>(endTime - startTime);
     cout << "Computing time=" << compTime.count() << " microseconds" << endl;
+    _mm_free(dat_real);
+    _mm_free(dat_imag);
+    _mm_free(pri_real);
+    _mm_free(pri_imag);
 
-    delete[] dat;
-    delete[] pri;
+    _mm_free(ctf);
+    _mm_free(sigRcp);
+    _mm_free(disturb);
 
-    delete[] ctf;
-    delete[] sigRcp;
-    delete[] disturb;
+    _mm_free(resBuffer);
+    _mm_free(result);
     return EXIT_SUCCESS;
 }
 
-double logDataVSPrior(const Complex* dat, const Complex* pri, const double* ctf, const double* sigRcp, const int num, const double disturb0)
-{
-    double result = 0.0;
-
-    for (int i = 0; i < num; i++)
-    {
-
-          result += ( norm( dat[i] - disturb0 * ctf[i] * pri[i] ) * sigRcp[i] );
-
+double logDataVSPrior(const double* dat_real,const double* dat_imag, const double* pri_real,const double* pri_imag, const double* ctf, const double* sigRcp,const int start, const int chunkSize, const double disturb,const int chunkId)
+{   
+    double result=0.0;
+    #pragma ivdep
+    #pragma unroll(8)
+    for (int i=start;i<start+chunkSize;i++){
+        double real=dat_real[i]-disturb*ctf[i]*pri_real[i];
+        double imag=dat_imag[i]-disturb*ctf[i]*pri_imag[i];
+        result+=(real*real+imag*imag)*sigRcp[i];
     }
     return result;
 }
